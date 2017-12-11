4f27256473c355509df01036c24d14153962fe3c
Christian Martinez <chrimartinez@csumb.edu>
2017-12-11 00:53:07 +0000

    moved db from localhost to remote db on clearDB

--- database.php
+++ database.php
@@ -7,10 +7,10 @@ function getDatabaseConnection()
 //    $password = "b4bb0fe0";
   //  $dbname="heroku_718defa20350e99";
     
-    $host = "localhost";
-    $username = "chrisumartinez";
-    $password = "";
-    $dbname="final_project";
+    $host = "us-cdbr-iron-east-05.cleardb.net";
+    $username = "bca5f27d7798a0";
+    $password = "a62cdacd";
+    $dbname="heroku_8a41c4f40f86f93";
 
 // Create connection
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
